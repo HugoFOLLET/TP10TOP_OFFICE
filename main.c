7bc93a96d75ae8e8e7e1ecd668001422d476d1a3
HugoFOLLET <hugofollet875@gmail.com>
2021-04-08 10:42:31 +0200

    TP FINI

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include <string.h>
 #include "prod.h"
 
+void lireCommande(char commande[29]);
+void cherche (int ref, char* label[50], float* prix);
+int stockage(int ref, int nombre);
+
 int lireProchaineCommande() //pour lire l'int contenu dans nextFact
 {
 FILE *f;
@@ -53,13 +57,13 @@ do //ce do while prend fin dès que fichier commandeXXXX.txt est absent
 	ficCommande=fopen(nomCommande,"rt");
 	if (ficCommande!=NULL)
 		{ // le fichier commandeNNNN.txt existe
-			printf("\n fichier %s present",nomCommande);
-			//lireCommande(nomCommande); // à vous de coder cette fonction lors de ce TP9
+			//printf("\nFichier %s présent\n",nomCommande);
+			lireCommande(nomCommande); // à vous de coder cette fonction lors de ce TP9
 			fclose(ficCommande);
 		}
 	else
 		{
-			printf("\n toutes les commandes presentes ont ete traitees.");
+			printf("\nToutes les commandes présentes ont été traitées.\n");
 			FILE *f=fopen("nextFact","w"); // on va ecrire la valeur de N dans enxtFact 
 			// pour 
 			fwrite(&N,1,sizeof(int),f);
@@ -72,19 +76,101 @@ do //ce do while prend fin dès que fichier commandeXXXX.txt est absent
 
 }
 
+void lireCommande(char commande[29]){
 
+	int i;
+	int ref, number;
+	char nom[30];
+	char label [50];
+	float prix;
+	float total=0;
+	char fact[29] = "./factures/factureXXXX.txt";
+	
+	for(i=18; i<22; i++) fact[i] = commande[i+2];
+	
+	FILE *g=fopen(fact, "w");
+	
+	//printf("%s Yo \n",commande);
+	FILE *f=fopen(commande,"r");
+	//fread(&nom,sizeof(nom),1,f);
+	fscanf(f, "%s\n", &nom);
+	
+	fprintf(g,"Client : %s\n\n", nom);
+	
+	do {
+		ref = 0;
+		fscanf(f, "%d %d\n", &ref, &number);
+		
+		if (!stockage(ref, number)){
+			FILE *h=fopen("./alertes.txt", "a");
+			fprintf(h, "%d\n", ref);
+			fclose(h);
+		}
+		
+		if (ref){
+			cherche(ref, &label, &prix);
+			//printf("Nom : %s\n", &label);
+			//printf("Prix : %f\n", prix*number);
+			total += prix*number;
+			
+			fprintf(g,"%d %s (PU = %.2f€) :: %.2f€\n", number, label, prix, prix*number);
+		}
+		//printf("Le total est de : %f\n", total);
+	} while (ref);
+	
+	fprintf(g,"\nTOTAL = %.2f\n", total);
+	
+	fclose(f);
+	fclose(g);
+}
+
+void cherche (int ref, char* label[50], float* prix){
+	int prod=0;
+	FILE *f=fopen("./produits.txt", "r");
+	while(prod!=ref){
+		fscanf(f, "%d %s %f\n", &prod, label, prix);
+	}
+}
+
+int stockage(int ref, int nombre){
+	int prod;
+	int stock;
+	int out = 0;
+	FILE *f=fopen("./stock.txt", "r");
+	FILE *g=fopen("./stock2.txt", "w");
+	do{
+		prod = 0;
+		fscanf(f, "%d %d\n", &prod, &stock);
+		if (prod == ref){
+			if (stock >= nombre){
+				stock -= nombre;
+				out = 1;
+			} else out = 0;
+			
+			
+		}
+		if (prod) fprintf(g, "%d %d\n", prod, stock);
+	} while(prod);
+	
+	fclose(f);
+	fclose(g);
+	
+	system("cp stock2.txt stock.txt");
+	
+	return out;
+	
+}
 
 int main()
 {
 	//creation d un fichier d'un seul int nommé nextFact et contenant l'int 1
 	// code à utiliser pour réinitialiser nextFact à 1 si besoin au cours du TP 
 	
-	/*
-	FILE *f;int N=1;
+	
+	/*FILE *f;int N=1;
 	f=fopen("nextFact","w");
 	fwrite(&N,1,sizeof(int),f);
-	fclose(f);
-	*/ 	
+	fclose(f);*/
 
 	//PARTIE 1 du TP : sans Gestion de stock
 	lireLesCommandes(); //lecture de tous les fichiers commandeXXX.txt (fichiers non traités jusqu'ici)	
